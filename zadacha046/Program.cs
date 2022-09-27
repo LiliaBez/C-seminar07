a6fb3caff9903253b5ce346f67800f3d83104531
Lilia Bezrukova <lilia_dyatlova@mail.ru>
2022-09-27 20:52:47 +0400

    zadacha046

---
+++ zadacha046/Program.cs
@@ -0,0 +1,42 @@
+﻿/*Задайте двумерный массив размером m×n, 
+заполненный случайными целыми числами.*/
+
+int height = EnterInt ("Enter height: "); //высота массива
+int width = EnterInt ("Enter widht: ");  //ширина массива
+
+int[ , ] numbers = new int[height, width];
+
+Fill2DArray(numbers, height, width);
+Print2dArray(numbers, height, width);
+
+
+int EnterInt(string prompt)
+{
+    Console.Write(prompt);
+    return int.Parse(Console.ReadLine()!);
+}
+
+void Fill2DArray (int[,] numbers, int height, int widht)
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            numbers[i, j] = new Random ().Next (-10, 11);
+        }
+    }
+}
+
+void Print2dArray (int[,] numbers, int height, int widht)
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            Console.Write ($"{numbers[i, j],3} ");
+        }
+        Console.WriteLine (); 
+    }
+}
+
+
