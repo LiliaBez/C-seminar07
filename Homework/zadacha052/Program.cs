ccedfc2847c7e91c3673c640372beca34b052c87
Lilia Bezrukova <lilia_dyatlova@mail.ru>
2022-09-27 22:50:59 +0400

    zadacha052

--- Homework/zadacha052/Program.cs
+++ Homework/zadacha052/Program.cs
@@ -5,24 +5,28 @@
 int[ , ] numbers = { { 1, 4 ,7, 2}, 
                      { 5, 9, 2, 3 }, 
                      { 8, 4, 2, 4} };
-int height = numbers.GetLength(0);
-int widht = numbers.GetLength(1);
 
-double FindArithmeticMeanColumn (int[,] numbers, int height, int widht)
+FindArithmeticMeanColumn(numbers);
+
+
+
+void FindArithmeticMeanColumn (int[,] numbers)
 {
+    int height = numbers.GetLength(0);
+    int widht = numbers.GetLength(1);
     for (int j = 0; j < widht; j++)
     {
-        double sum = 0;
+        int sum = 0;
         for (int i = 0; i < height; i++)
         {
             sum = sum + numbers[i, j];
         }
-        return sum / height;
+        float arithmeticMean = sum / height;
+        Console.WriteLine($"Среднее арифметическое элементов столбца {j} равно: {arithmeticMean}");
     }
-    return Console.WriteLine(sum / height);
 }
 
-Console.Write(FindArithmeticMeanColumn);
+
 
 
 
