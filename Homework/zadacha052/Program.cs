b6ad62dd6b7c947b81e5ee1c769c35e8dcaed580
Lilia Bezrukova <lilia_dyatlova@mail.ru>
2022-09-27 20:51:30 +0400

    zadacha052

---
+++ Homework/zadacha052/Program.cs
@@ -0,0 +1,28 @@
+﻿/*Задайте двумерный массив из целых чисел. 
+Найдите среднее арифметическое элементов в каждом столбце.*/
+
+
+int[ , ] numbers = { { 1, 4 ,7, 2}, 
+                     { 5, 9, 2, 3 }, 
+                     { 8, 4, 2, 4} };
+int height = numbers.GetLength(0);
+int widht = numbers.GetLength(1);
+
+double FindArithmeticMeanColumn (int[,] numbers, int height, int widht)
+{
+    for (int j = 0; j < widht; j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < height; i++)
+        {
+            sum = sum + numbers[i, j];
+        }
+        return sum / height;
+    }
+    return Console.WriteLine(sum / height);
+}
+
+Console.Write(FindArithmeticMeanColumn);
+
+
+
